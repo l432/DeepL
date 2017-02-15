b468a23c0619913b73d53f4a56842c6e4a77c772
Oleg <l432432@gmail.com>
2017-02-15 17:19:02 +0200

    15-02-2017

--- Boteon/Lesson3/RGBdiod/RGBdiod.ino
+++ Boteon/Lesson3/RGBdiod/RGBdiod.ino
@@ -6,55 +6,55 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(RED_PIN, 255);
-  digitalWrite(GREEN_PIN, 0);
-  digitalWrite(BLUE_PIN, 0);
+  analogWrite(RED_PIN, 255);
+ analogWrite(GREEN_PIN, 0);
+  analogWrite(BLUE_PIN, 0);
   delay(1000);
 
-  digitalWrite(RED_PIN, 0);
-  digitalWrite(GREEN_PIN, 255);
-  digitalWrite(BLUE_PIN, 0);
+  analogWrite(RED_PIN, 0);
+  analogWrite(GREEN_PIN, 255);
+  analogWrite(BLUE_PIN, 0);
   delay(1000);
 
-  digitalWrite(RED_PIN, 0);
-  digitalWrite(GREEN_PIN, 0);
-  digitalWrite(BLUE_PIN, 255);
+  analogWrite(RED_PIN, 0);
+  analogWrite(GREEN_PIN, 0);
+  analogWrite(BLUE_PIN, 255);
   delay(1000);
 
   //red
-  digitalWrite(RED_PIN, 245);
-  digitalWrite(GREEN_PIN, 50);
-  digitalWrite(BLUE_PIN, 11);
+  analogWrite(RED_PIN, 245);
+  analogWrite(GREEN_PIN, 50);
+  analogWrite(BLUE_PIN, 11);
   delay(1000);
 
   //orange
-  digitalWrite(RED_PIN, 252);
-  digitalWrite(GREEN_PIN, 158);
-  digitalWrite(BLUE_PIN, 4);
+  analogWrite(RED_PIN, 252);
+  analogWrite(GREEN_PIN, 158);
+  analogWrite(BLUE_PIN, 4);
   delay(1000);
 
   //yellow
-  digitalWrite(RED_PIN, 255);
-  digitalWrite(GREEN_PIN, 255);
-  digitalWrite(BLUE_PIN, 0);
+  analogWrite(RED_PIN, 255);
+  analogWrite(GREEN_PIN, 255);
+  analogWrite(BLUE_PIN, 0);
   delay(1000);
 
   //green
-  digitalWrite(RED_PIN, 33);
-  digitalWrite(GREEN_PIN, 245);
-  digitalWrite(BLUE_PIN, 11);
+  analogWrite(RED_PIN, 33);
+  analogWrite(GREEN_PIN, 245);
+  analogWrite(BLUE_PIN, 11);
   delay(1000);
 
   //blue
-  digitalWrite(RED_PIN, 31);
-  digitalWrite(GREEN_PIN, 15);
-  digitalWrite(BLUE_PIN, 241);
+  analogWrite(RED_PIN, 31);
+  analogWrite(GREEN_PIN, 15);
+  analogWrite(BLUE_PIN, 241);
   delay(1000);
 
   //violent
-  digitalWrite(RED_PIN, 153);
-  digitalWrite(GREEN_PIN, 0);
-  digitalWrite(BLUE_PIN, 255);
+  analogWrite(RED_PIN, 153);
+  analogWrite(GREEN_PIN, 0);
+  analogWrite(BLUE_PIN, 255);
   delay(1000);
 
 }
